d25c51ce9d3fe6bfbaae7bf0d36ff421e71a9065
rajmaddheshiya599-bit <rajmaddheshiya599@gmail.com>
2026-06-18 08:53:35 +0530

    Completed

---
+++ week2/Day-5/Longest_Palindromic_substring.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.length();
+        if (n == 0) return "";
+
+        int start = 0, maxLen = 1;
+
+        for (int i = 0; i < n; i++) {
+            // Odd length palindrome
+            int l = i, r = i;
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                if (r - l + 1 > maxLen) {
+                    maxLen = r - l + 1;
+                    start = l;
+                }
+                l--;
+                r++;
+            }
+
+            // Even length palindrome
+            l = i;
+            r = i + 1;
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                if (r - l + 1 > maxLen) {
+                    maxLen = r - l + 1;
+                    start = l;
+                }
+                l--;
+                r++;
+            }
+        }
+
+        return s.substr(start, maxLen);
+    }
+};
