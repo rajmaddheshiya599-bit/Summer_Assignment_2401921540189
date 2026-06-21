4b23fceac22c6fb01263a29415741312c47614b7
rajmaddheshiya599-bit <rajmaddheshiya599@gmail.com>
2026-06-21 08:43:27 +0530

    Completed

--- week3/Day-2/Palindrome_linkedlist.cpp
+++ week3/Day-2/Palindrome_linkedlist.cpp
@@ -6,32 +6,34 @@ struct ListNode {
     ListNode *next;
     ListNode(int x) : val(x), next(NULL) {}
 };
+
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        vector<int>v;
-        bool flag = false;
-        ListNode* temp= head;
-        int i =0 ;
-        while(temp!=NULL){
+
+        vector<int> v;
+        ListNode* temp = head;
+
+        while(temp != NULL){
             v.push_back(temp->val);
             temp = temp->next;
         }
-        temp= head;
-        reverse(v.begin(),v.end());
-        while(temp!=NULL){
-            if(v[i]==temp->val){
-            i++;
-            temp = temp->next;
-            continue;
-                
+
+        reverse(v.begin(), v.end());
+
+        temp = head;
+        int i = 0;
+
+        while(temp != NULL){
+            if(v[i] == temp->val){
+                i++;
+                temp = temp->next;
+            }
             else{
-            return false;
-            
+                return false;
+            }
         }
-    if(temp == NULL){
-        flag = true;
-    }
-    return flag;
+
+        return true;
     }
 };
--- week3/Day-2/Remove_Nth_Node_fromendoflist.cpp
+++ week3/Day-2/Remove_Nth_Node_fromendoflist.cpp
@@ -1,21 +1,60 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+
+    ListNode() {
+        val = 0;
+        next = nullptr;
+    }
+
+    ListNode(int x) {
+        val = x;
+        next = nullptr;
+    }
+
+    ListNode(int x, ListNode* next) {
+        val = x;
+        this->next = next;
+    }
+};
+
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* temp = head;
+
+        if (head == nullptr) return nullptr;
+
         int size = 0;
-        while(temp!= NULL){
+        ListNode* temp = head;
+
+        while (temp != nullptr) {
             size++;
             temp = temp->next;
         }
+
+        int newidx = size - n + 1;
+
+        // First node delete karna hai
+        if (newidx == 1) {
+            ListNode* delNode = head;
+            head = head->next;
+            delete delNode;
+            return head;
+        }
+
         temp = head;
-        int newidx = size-n+1;
-        if(newidx == 1){
-            return head->next;
+
+        for (int i = 1; i < newidx - 1; i++) {
+            temp = temp->next;
         }
-        for(int i=1; i<newidx-1; i++){
-            temp= temp->next;
-        }
+
+        ListNode* delNode = temp->next;
         temp->next = temp->next->next;
-    return head;
+        delete delNode;
+
+        return head;
     }
 };
