4d186f7ddb365e08dd3a7a544870055d988c2812
rajmaddheshiya599-bit <rajmaddheshiya599@gmail.com>
2026-06-21 08:55:04 +0530

    Completed

--- week3/Day-3/MIn_Stack.cpp
+++ week3/Day-3/MIn_Stack.cpp
@@ -1,28 +1,62 @@
-class MinStack {
-public:
-    stack<int> st, mn;
+#include <iostream>
+#include <stack>
+using namespace std;
 
+class MinStack {
+private:
+    stack<int> st;
+    stack<int> mn;
+
+public:
     MinStack() {}
 
     void push(int val) {
         st.push(val);
 
-        if (mn.empty())
+        if (mn.empty()) {
             mn.push(val);
-        else
+        } else {
             mn.push(min(val, mn.top()));
+        }
     }
 
     void pop() {
-        st.pop();
-        mn.pop();
+        if (!st.empty()) {
+            st.pop();
+            mn.pop();
+        }
     }
 
     int top() {
+        if (st.empty()) {
+            cout << "Stack is empty\n";
+            return -1;
+        }
         return st.top();
     }
 
     int getMin() {
+        if (mn.empty()) {
+            cout << "Stack is empty\n";
+            return -1;
+        }
         return mn.top();
     }
 };
+
+int main() {
+    MinStack obj;
+
+    obj.push(-2);
+    obj.push(0);
+    obj.push(-3);
+
+    cout << "Minimum: " << obj.getMin() << endl;
+
+    obj.pop();
+
+    cout << "Top: " << obj.top() << endl;
+    cout << "Minimum: " << obj.getMin() << endl;
+
+    return 0;
+}
